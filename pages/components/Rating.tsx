0e75c592d6c0e6929372b65950a3360b66a5f651
jared brown <jaredbrown1@hotmail.com>
2023-04-17 15:26:34 -0700

    cleaned up - initial commit

---
+++ pages/components/Rating.tsx
@@ -0,0 +1,18 @@
+const Rating = () => {
+  return (
+    <form>
+      <div>
+        <div>star</div>
+      </div>
+      <h1>How did we do?</h1>
+      <p>
+        Please let us know how we did with your support request. All feedback is
+        appreciated to help us improve our offering!
+      </p>
+      <div>Number ratings here</div>
+      <button>Submit</button>
+    </form>
+  );
+};
+
+export default Rating;
